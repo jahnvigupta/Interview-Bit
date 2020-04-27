69b29f9b4ca95809dd7e34f70b28c7730e857c73
Ashutosh Sharma <ashutoshsharma0061@gmail.com>
2020-04-27 20:05:59 +0530

    .

---
+++ Bit Manipulation/singleintegerII.cpp
@@ -0,0 +1,14 @@
+int Solution::singleNumber(const vector<int> &A) {
+    int ans=0;
+    for(int i=0;i<32;i++){
+        int cnt=0;
+        for(int j=0;j<A.size();j++){
+            if(A[j]& (1<<i))
+                cnt++;
+        }
+        if(cnt%3!=0)
+            ans|=(1<<i);
+    }
+    return ans;
+}
+
